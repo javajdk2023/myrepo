bfd5e8c928735f21547b05f4b76557b9a29daafb
javajdk2023@gmail.com <javajdk2023@gmail.com>
2024-09-15 17:21:24 -0300

    my github action

--- myapp/src/main/java/com/example/demo/MyappApplication.java
+++ myapp/src/main/java/com/example/demo/MyappApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MyappApplication {
@@ -10,4 +13,20 @@ public class MyappApplication {
 		SpringApplication.run(MyappApplication.class, args);
 	}
 
+}
+
+//Controlador REST
+@RestController
+@RequestMapping("/api")
+class MyController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello, World!";
+	}
+
+	@GetMapping("/greet")
+	public String greetUser(String name) {
+		return "Hello, " + (name != null ? name : "Guest") + "!";
+	}
 }
